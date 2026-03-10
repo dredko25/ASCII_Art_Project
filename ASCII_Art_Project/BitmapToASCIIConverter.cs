0cae5a407fa841023042fd8a67e361ad6ed27ca8
dredko25 <dredko25@gmail.com>
2026-03-10 14:51:34 +0200

    Add full project files.

---
+++ ASCII_Art_Project/BitmapToASCIIConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Art_Project
+{
+    internal class BitmapToASCIIConverter
+    {
+        private readonly Bitmap _bitmap;
+        private readonly char[] _acsiiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+        
+        public BitmapToASCIIConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+
+        }
+
+        public char[] Reverse()
+        {
+            char[] reversedTable = new char[_acsiiTable.Length];
+            for (int i = 0; i < _acsiiTable.Length; i++)
+            {
+                reversedTable[i] = _acsiiTable[_acsiiTable.Length - 1 - i];
+            }
+            return reversedTable;
+        }
+
+        public char[][] Convert()
+        {
+            return Convert(_acsiiTable);
+        }
+
+        public char[][] ConvertReversed()
+        {
+            return Convert(Reverse());
+        }
+
+        public char[][] Convert(char[] asciiTable)
+        {
+            var result = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                result[y] = new char[_bitmap.Width];
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, asciiTable.Length - 1);
+                    result[y][x] = asciiTable[mapIndex];
+                }
+            }
+            return result;
+        }
+
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
--- ASCII_Art_Project/Program.cs
+++ ASCII_Art_Project/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using System.IO;
+
 
 namespace ASCII_Art_Project
 {
     internal class Program
     {
+        private const double WIDTH_OFFSET = 1.7;
+        private const int MAX_WIDTH = 474;
+
+        [STAThread]
         static void Main(string[] args)
         {
+            OpenFileDialog openFileDilog = new OpenFileDialog
+            {
+                Filter = "Images|*.jpg;*.jpeg;*.png;*.bmp"
+            };
+
+            do
+            {
+                if (openFileDilog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                Bitmap bitmap = new Bitmap(openFileDilog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+
+                bitmap.ToGrayscale();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+
+                foreach (var row in rows)
+                    Console.WriteLine(row);
+
+                var rowReversed = converter.ConvertReversed();
+                File.WriteAllLines("image.txt", rowReversed.Select(r => new string(r)));
+                
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Press enter to add new image... \n");
+                Console.ReadLine();
+
+            } while (true);
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+            return bitmap;
         }
     }
 }
